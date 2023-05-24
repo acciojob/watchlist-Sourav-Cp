1f4ea686510a1735ae45a0c93a1ddc84161d712a
Sourav <sourav10singh11@gmail.com>
2023-05-24 19:42:01 +0530

    Last Try

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 @RestController
-@RequestMapping("movies")
+@RequestMapping("/movies")
 public class MovieController {
 
     private MovieService movieService = new MovieService();
